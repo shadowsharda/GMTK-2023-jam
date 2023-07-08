ebaf3a9422c901e47bebcb19bd79f8b07190862a
iceofspade <iceofspade@outlook.com>
2023-07-07 20:09:20 -0500

    Base Project commit

---
+++ scenes/menue.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_up():
+	get_tree().change_scene_to_file("res://scenes/test_scene.tscn")
+	pass # Replace with function body.
+
+
+func _on_settings_button_up():
+	$Settings.visible = true
+	pass # Replace with function body.
+
+
+func _on_quit_button_up():
+	get_tree().quit()
+	pass # Replace with function body.
---
+++ scenes/settings.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_button_up():
+	visible = false
+	pass # Replace with function body.
+
+
+func _on_sound_h_slider_value_changed(value):
+	print(value)
+	pass # Replace with function body.
+
+
+func _on_volume_h_slider_value_changed(value):
+	print(value)	
+	pass # Replace with function body.
